73b748e735b333ebb516bfa4f862e9c763702f23
Carl Masak <cmasak@gmail.com>
2009-01-31 23:18:24 +0100

    fixed type inconsistent with 'handles'
    It's not Druid::Game::Subject that owns those methods, it's Druid::Game_

--- lib/Druid/View.pm
+++ lib/Druid/View.pm
@@ -1,7 +1,7 @@
 use v6;
-use Druid::Game::Subject;
+use Druid::Game;
 use Druid::Game::Observer;
 
 class Druid::View does Druid::Game::Observer {
-    has Druid::Game::Subject $!game handles <size layers colors heights>;
+    has Druid::Game_ $!game handles <size layers colors heights>;
 }
