284af54996e4c49a5a57d9159d121719a856a641
Carl Masak <cmasak@gmail.com>
2009-04-12 23:49:04 +0200

    [Druid::Webapp] factored out web app logic

---
+++ lib/Druid/Webapp.pm
@@ -0,0 +1,26 @@
+use v6;
+use Tags; 
+use Druid::Game; 
+use Druid::View::Text;
+
+class Druid::Webapp {
+
+    method page(Str $query-string) {
+        my $board-size = 8;
+        my Druid::Game $game .= new(:size($board-size));
+        my Druid::View $view = Druid::View::Text.new(:$game);
+
+        return
+            show {
+                html {
+                    head { title { 'Druid' } }
+                    body {
+                        pre { $view }
+                        ul {
+                            for $game.possible-moves() -> $move {
+                                li {
+                                    a :href("?moves=$move"), { $move }
+            }}}}}};
+    }
+
+}
