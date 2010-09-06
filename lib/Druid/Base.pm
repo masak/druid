86de6dfaa7b0ad383417f68e4b235e1e6f5bb720
Carl Masak <cmasak@gmail.com>
2010-09-06 11:18:22 +0200

    [Druid::Base] worked around Rakudo parsefail
    Rakudo currently can't parse comments starting with '#= '. Changing to
    '# = ' for the time being.

--- lib/Druid/Base.pm
+++ lib/Druid/Base.pm
@@ -1,4 +1,4 @@
-#= Base class collecting ambient regexes, attributes and methods.
+# = Base class collecting ambient regexes, attributes and methods.
 class Druid::Base;
 
 # RAKUDO: Cannot use dashes here. [perl #64464]
@@ -6,13 +6,13 @@ regex col_letter { <[a..z]> }
 regex row_number { \d+ }
 regex coords { <col_letter><row_number> }
 
-our $.sarsen-move = /^ <coords> $/;              #= A sarsen (length 1) move
-our $.lintel-move = /^ <coords> '-' <coords> $/; #= A lintel (length 3) move
-our $.pass   = /^ ['pass'   | 'p'] $/;           #= A passing (no-op) move
-our $.swap   = /^ ['swap'   | 's'] $/;           #= A swap move
-our $.resign = /^ ['resign' | 'r'] $/;           #= A forfeit
+our $.sarsen-move = /^ <coords> $/;              # = A sarsen (length 1) move
+our $.lintel-move = /^ <coords> '-' <coords> $/; # = A lintel (length 3) move
+our $.pass   = /^ ['pass'   | 'p'] $/;           # = A passing (no-op) move
+our $.swap   = /^ ['swap'   | 's'] $/;           # = A swap move
+our $.resign = /^ ['resign' | 'r'] $/;           # = A forfeit
 
-#= Returns (zero-based) row and column, given a C<Match> object
+# = Returns (zero-based) row and column, given a C<Match> object
 method extract-coords(Match $m) {
     # RAKUDO: Hoping these explicit (...).Int conversions won't be
     #         necessary in the long run.
